89b0312ce94a51c65ff1922b4bc5300e6aec685e
Marcin Pilarczyk <marcinpilarczyk@me.com>
2023-11-08 09:25:57 +0100

    Upload source

---
+++ src/Pacco.Services.Parcels.Core/Exceptions/InvalidParcelDescriptionException.cs
@@ -0,0 +1,11 @@
+namespace Pacco.Services.Parcels.Core.Exceptions;
+
+public class InvalidParcelDescriptionException : DomainException
+{
+    public override string Code { get; } = "invalid_parcel_description";
+
+    public InvalidParcelDescriptionException(string description)
+        : base($"Parcel description is invalid: {description}.")
+    {
+    }
+}
